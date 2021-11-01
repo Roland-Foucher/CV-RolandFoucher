bc9b6b47d51a288060a4f1ee83a48c60880a33d5
Roland Foucher <roland-foucher@hotmail.fr>
2021-11-01 15:43:03 +0100

    ValidateEntry OK

--- script/Jquery.js
+++ script/Jquery.js
@@ -1,5 +1,24 @@
 
-$("#sendMessage").submit(
+const regexStd = /^.+/;
+const regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+
+const $nom = $("#nom");
+const $prenom = $("#prenom")
+const $mail = $("#mail");
+const $sendMessage = $("#sendMessage");
+
+function validateEntry(dom, reg) {
+    reg.test(dom.val()) ? dom.css({'background-color' : 'rgba(0,255,0,0.5)'}) : dom.css({'background-color' : 'rgba(255,0,0,0.5)'})
+}
+
+$nom.change (() => validateEntry($nom, regexStd))
+$prenom.change (() => validateEntry($prenom, regexStd))
+$mail.change (() => validateEntry($mail, regexEmail))
+
+
+     
+
+$sendMessage.submit(
     (e) => {
         alert("Ce formulaire est fictif 😅");
         e.preventDefault();
